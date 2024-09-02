fb58baa3e6bcd28f08b084357e0d1a30f457a2ad
Harry <henriquecbgamer@gmail.com>
2024-09-02 00:02:59 -0300

    feat: Estrutura basica

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import NavBar from "@/components/NavBar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <NavBar/>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    </main>
+    </>
+  );
+}
