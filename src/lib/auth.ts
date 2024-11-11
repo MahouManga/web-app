00b744c0195a0fb088c91a1d62fd6ff022d5b557
Harry <henriquecbgamer@gmail.com>
2024-11-11 16:15:48 -0300

    hmm se pá?

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -9,7 +9,7 @@ export const lucia = new Lucia(adapter, {
 	sessionCookie: {
 		expires: false,
 		attributes: {
-			secure: process.env.NODE_ENV === "production"
+			secure: false, //process.env.NODE_ENV === "production",
 		}
 	},
 	getUserAttributes: (attributes) => {
